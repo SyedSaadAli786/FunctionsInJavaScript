c2363ef3b7c62d12dfc8a5b15aa3fa9af9ade42d
Syed Saad Ali <saadali23489@gmail.com>
2022-07-20 20:08:52 +0500

    funtions in javascript

---
+++ function.js
@@ -0,0 +1,18 @@
+//       FUNCTION
+
+function greet(member, greetText){
+    console.log(member + " is good boy ");
+    console.log(member + " " + greetText);
+}
+
+let name1 = "Saad"  ;
+let name2 = "Ali"   ;
+let name3 = "Maqsood";
+let name4 = "Ahmed";
+let greetText = "good morning"
+
+greet(name1 , greetText);
+greet(name2 , greetText);
+greet(name3 , greetText);
+greet(name4 , greetText);
+
